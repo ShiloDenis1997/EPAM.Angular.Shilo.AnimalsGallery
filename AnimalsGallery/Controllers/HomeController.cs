e12d603ac3b1529726f0b331a00ce7f4cfa42602
Shilo Denis <shilo.denis.1997g@gmail.com>
2017-03-20 02:01:17 +0300

    routing, gallery, albums and about changin on client size added

---
+++ AnimalsGallery/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnimalsGallery.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        public ActionResult Albums()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
